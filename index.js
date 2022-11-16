312a3934ebcaf5ec656e9843b5c2fb18c8fe5ee2
mehedititu <mh.me.mehedi@gmail.com>
2022-11-16 21:08:41 +0600

    API done for avilable patient

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// midleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.USER_SECRET}@cluster0.k4gmzpi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run () {
+    try {
+        const appointmentData = client.db("doctorsPortal").collection("slots");
+        const appointmentBooking = client.db("doctorsPortal").collection("booking");
+        app.get('/apointmentOptions', async(req, res) => {
+            const date = req.query.date;
+            const query = {};
+            const cursor = appointmentData.find(query);
+            const options = await cursor.toArray();
+
+
+            const appointmentQuery = {appointmentDate: date}
+            const alreadyBooked = await appointmentBooking.find(appointmentQuery).toArray();
+            options.forEach(option => {
+                const booked = alreadyBooked.filter(book => book.service === option.name);
+                const bookSlot = booked.map(book => book.time);
+                const reminingSlot = option.slots.filter( slot => !bookSlot.includes(slot));
+                option.slots = reminingSlot;
+            });
+            res.send(options);
+        })
+
+        // Appointment Booking
+        app.post('/booking', async(req, res) => {
+            const body = req.body;
+
+
+            const query = {
+                appointmentDate: body.appointmentDate,
+                service: body.service,
+                email: body.email,
+            }
+            const bookingDone = await appointmentBooking.find(query).toArray();
+            if(bookingDone.length) {
+                const message = `You have a booked on ${body.appointmentDate}`;
+                return res.send({acknowledged: false, message});
+            }
+
+
+            const result = await appointmentBooking.insertOne(body);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Doctors portal server running with: ${port}`)
+})
